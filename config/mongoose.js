c090c99138cb0cdf44d72ec29e7546953dd23179
James Yoder <jamesdeanyoder@gmail.com>
2017-03-21 13:54:41 -0400

    Finished the angular portion of the tutorial. Tutorial complete, front end, back end and database.

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,11 @@
 var config = require('./config'),
-    mongoose = require('mongoose');
+	mongoose = require('mongoose');
 
 module.exports = function() {
-    var db = mongoose.connect(config.db);
-    require('../app/models/users.server.model');
-    return db;
-}
+	var db = mongoose.connect(config.db);
+
+	require('../app/models/user.server.model');
+	require('../app/models/todo.server.model');
+
+	return db;
+};
